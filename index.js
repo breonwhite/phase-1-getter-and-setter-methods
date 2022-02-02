776fccf0b6bf9ce5d027883bfb4806bb89bda7a3
Breon White <bdwhite92@gmail.com>
2022-02-02 15:38:55 -0500

    commit tree 7789f75d6e58c92b6ab9d36a7684e8372c2d6f6c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return Math.PI * (this.radius * 2)
+    }
+
+    get area() {
+        return Math.PI * (Math.pow(this.radius, 2))
+    }
+
+    set diameter(newDiameter){
+        this.radius = newDiameter/2;
+    }
+
+    set circumference(newCircumference) {
+        this.radius = (newCircumference/Math.PI)/2;
+    }
+
+    set area(newArea) {
+        this.radius = Math.sqrt(newArea/Math.PI);
+    }
+
+
+}
+
+
+const rollie = new Circle(12);
+console.log(rollie.diameter);
